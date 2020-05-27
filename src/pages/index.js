244b8e8975859160a95526ec4f5b82ad372fa243
trnd <4416938+trndee@users.noreply.github.com>
2020-05-27 18:24:49 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,8 @@ import Navbar from '@common/Navbar';
 
 import Header from '@sections/Header';
 import About from '@sections/About';
-import Brands from '@sections/Brands';
-import Team from '@sections/Team';
-import Faq from '@sections/Faq';
+import Brands from '@sections/FAQ';
+import Team from '@sections/Contact';
 import Footer from '@sections/Footer';
 
 const IndexPage = () => (
@@ -15,9 +14,8 @@ const IndexPage = () => (
     <Navbar />
     <Header />
     <About />
-    <Brands />
-    <Team />
     <Faq />
+    <Contact />
     <Footer />
   </Layout>
 );
