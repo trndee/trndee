aa195b8121ca2df84fe59d031029a671863069b9
trnd <4416938+trndee@users.noreply.github.com>
2020-05-27 20:21:46 +0200

    Update Footer.js

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -43,17 +43,11 @@ const Footer = () => (
     `}
     render={data => (
       <React.Fragment>
-        <Art>
-          <Img
-            fluid={data.art_pot.childImageSharp.fluid}
-            style={{ width: 480, maxWidth: '100%', marginBottom: -16 }}
-          />
-        </Art>
         <FooterWrapper>
           <StyledContainer>
             <Copyright>
               <p>
-                &copy; trnd.ee OÜ. All rights reserved.
+                &#169; trnd.ee OÜ. All rights reserved.
               </p>
             </Copyright>
           </StyledContainer>
