26a5a806a742200e28d4f247898e1b6d9f0444a5
trnd <4416938+trndee@users.noreply.github.com>
2020-05-27 17:30:26 +0200

    Update Navbar.js

--- src/components/common/Navbar/Navbar.js
+++ src/components/common/Navbar/Navbar.js
@@ -15,7 +15,7 @@ import {
 
 import { ReactComponent as MenuIcon } from '@static/icons/menu.svg';
 
-const NAV_ITEMS = ['About', 'Brands', 'Team', 'FAQ'];
+const NAV_ITEMS = ['Home', 'About', 'FAQ', 'Contact'];
 
 class Navbar extends Component {
   state = {
