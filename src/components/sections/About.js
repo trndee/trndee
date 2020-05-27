33fb7b1d658f6ba14676380300e8e8f5fb7aa9ee
trnd <4416938+trndee@users.noreply.github.com>
2020-05-27 18:37:49 +0200

    Update About.js

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -48,12 +48,11 @@ const About = () => (
         <Container>
           <Grid>
             <div>
-              <h2>Speed past the competition</h2>
+              <h2>What</h2>
               <p>
-                Gatsby.js builds the fastest possible website. Instead of
-                waiting to generate pages when requested, pre-build pages and
-                lift them into a global cloud of servers — ready to be delivered
-                instantly to your users wherever they are.
+                We extract information from data to predict trends patterns and 
+                we calculate the statistical probabilities of future events to 
+                shape alternative futures.
               </p>
             </div>
             <Art>
@@ -65,23 +64,19 @@ const About = () => (
               <Img fluid={data.art_learn.childImageSharp.fluid} />
             </Art>
             <div>
-              <h2>Nothing new to learn here</h2>
+              <h2>How</h2>
               <p>
-                Enjoy the power of the latest web technologies – React.js ,
-                Webpack , modern JavaScript and CSS and more — all set up and
-                waiting for you to start building.
+                Our techniques include data modeling, machine learning, 
+                AI, deep learning algorithms and data mining.
               </p>
             </div>
           </Grid>
           <Grid>
             <div>
-              <h2>Grow and build your ideas</h2>
+              <h2>Why</h2>
               <p>
-                Waste no more time on tooling and performance. Focus on the the
-                site you want to build and nothing more.
-                <br />
-                <br />
-                Gatsby is fast in every way that matters.
+                We use forecasting in many applications including anomaly detection, 
+                capacity planning, social networking and sports.
               </p>
             </div>
             <Art>
