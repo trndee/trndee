8e7b0456d1f7256ba6bd94c9928940415303a852
trnd <4416938+trndee@users.noreply.github.com>
2020-05-28 11:52:48 +0200

    Update Footer.js

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
-import StaticQuery from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 import { Container } from '@components/global';
+import ExternalLink from '@common/ExternalLink';
+
+import GithubIcon from '@static/icons/github.svg';
+import InstagramIcon from '@static/icons/instagram.svg';
+import TwitterIcon from '@static/icons/twitter.svg';
+
+const SOCIAL = [
+  {
+    icon: GithubIcon,
+    link: 'https://github.com/ajayns/gatsby-absurd',
+  },
+  {
+    icon: InstagramIcon,
+    link: 'https://instagram.com/ajay_ns',
+  },
+  {
+    icon: TwitterIcon,
+    link: 'https://twitter.com/ajayns08',
+  },
+];
 
 const Footer = () => (
   <StaticQuery
+    query={graphql`
+      query {
+        art_pot: file(
+          sourceInstanceName: { eq: "art" }
+          name: { eq: "customers_pot" }
+        ) {
+          childImageSharp {
+            fluid(maxWidth: 960) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    `}
     render={data => (
       <React.Fragment>
         <FooterWrapper>
@@ -22,6 +57,20 @@ const Footer = () => (
   />
 );
 
+const SocialIcons = styled.div`
+  display: flex;
+
+  img {
+    margin: 0 8px;
+    width: 24px;
+    height: 24px;
+  }
+
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    margin-top: 40px;
+  }
+`;
+
 const FooterWrapper = styled.footer`
   background-color: ${props => props.theme.color.primary};
   padding: 32px 0;
@@ -36,6 +85,13 @@ const Copyright = styled.div`
     text-decoration: none;
     color: inherit;
   }
+`;
+
+const Art = styled.figure`
+  display: flex;
+  justify-content: center;
+  margin: 0;
+  margin-top: 48px;
 `;
 
 const StyledContainer = styled(Container)`
