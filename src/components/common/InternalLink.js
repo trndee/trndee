8e8164683e3bd4a0fa06ab54d233d729941242fe
trnd <4416938+trndee@users.noreply.github.com>
2020-05-28 02:32:50 +0200

    Delete InternalLink.js

--- src/components/common/InternalLink.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const InternalLink = ({ href, children, ...other }) => (
-  <a href={href} {...other} rel="noreferrer noopener" target="_self">
-    {children}
-  </a>
-);
-
-export default InternalLink;
