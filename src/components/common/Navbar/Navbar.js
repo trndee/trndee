2db4fe1c5b5a8a6583707235c74778c817b4419c
trnd <4416938+trndee@users.noreply.github.com>
2020-05-28 11:22:21 +0200

    Update Navbar.js

--- src/components/common/Navbar/Navbar.js
+++ src/components/common/Navbar/Navbar.js
@@ -59,7 +59,7 @@ class Navbar extends Component {
     return (
       <Nav {...this.props}>
         <StyledContainer>
-          <Brand>&#x25CF;trnd</Brand>
+          <Brand>trnd</Brand>
           <Mobile>
             <button onClick={this.toggleMobileMenu} style={{ color: 'black' }}>
               <MenuIcon />
